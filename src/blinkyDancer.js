21943b39971b2df898321fa6115e3ed16738f3f4
Henri Pietila <henrismail@gmail.com>
2017-05-22 20:29:26 -0700

    fixed timeout

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -1,15 +1,19 @@
 var MakeBlinkyDancer = function(top, left, timeBetweenSteps) {
   //var blinkyDancer = makeDancer(top, left, timeBetweenSteps);
   //MakeDancer.call(this, top, left, timeBetweenSteps);
+  this.timeBetweenSteps = timeBetweenSteps;
+
   MakeDancer.apply(this, arguments);
+  //console.log(timeBetweenSteps);
+
   // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
   // so we must keep a copy of the old version of this function
-  
+
   // this.oldStep = blinkyDancer.step;
   // console.log(this);
   // console.log(this.step);
+  // console.log(timeBetweenSteps);
 
-  
 
 };
 
@@ -20,6 +24,7 @@ MakeBlinkyDancer.prototype.oldStep = MakeDancer.prototype.step;
 MakeBlinkyDancer.prototype.step = function() {
   // call the old version of step at the beginning of any call to this new version of step
   //console.log(this.oldStep.bind(this));
+  //console.log(this.timeBetweenSteps);
   this.oldStep.bind(this);
   this.oldStep();
   // toggle() is a jQuery method to show/hide the <span> tag.
@@ -27,6 +32,3 @@ MakeBlinkyDancer.prototype.step = function() {
   // other effects you can use on a jQuery-wrapped html tag.
   this.$node.toggle();
 };
-
-
-
--- src/dancer.js
+++ src/dancer.js
@@ -7,7 +7,7 @@ var MakeDancer = function(top, left, timeBetweenSteps) {
   this.$node = $('<span class="dancer"></span>');
   this.step();
 
-  
+
 
   // now that we have defined the dancer object, we can start setting up important parts of it by calling the methods we wrote
   // this one sets the position to some random default point within the body
@@ -19,9 +19,10 @@ var MakeDancer = function(top, left, timeBetweenSteps) {
 MakeDancer.prototype.step = function() {
   // the basic dancer doesn't do anything interesting at all on each step,
   // it just schedules the next step
- 
-  console.log(this.step);
-  setTimeout(this.step.bind(this), this.timeBetweenSteps);
+
+//  console.log(this.step);
+  //console.log(this.timeBetweenSteps);
+  setTimeout(this.step.bind(this), this.timeBetweenSteps);//this.timeBetweenSteps);
 
 };
 
